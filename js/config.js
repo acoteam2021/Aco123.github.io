d792771c0da54619beb3aef245feb8dd999a6b41
ACO Team <114649685+acoteam2021@users.noreply.github.com>
2023-05-14 00:58:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,12 @@
 
+// js/config.js
+
+var config = {
+  apiKey: "AIzaSyCyymDf0lxFQPNu04pWzl4BKjB3LBSlOY0",
+  authDomain: "nino-1429c.firebaseapp.com",
+  projectId: "nino-1429c",
+  storageBucket: "nino-1429c.appspot.com",
+  messagingSenderId: "889240089074",
+  appId: "1:889240089074:web:524f61f1954269d49bbb6b",
+  measurementId: "G-CL8BGWM161"
+};
